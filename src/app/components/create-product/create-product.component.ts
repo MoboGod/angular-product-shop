ddba69b0aab51ff6758cb9f818bb2ccb8830c3a7
Denis <d.rotari17@gmail.com>
2022-08-02 21:37:56 +0300

    create product

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.css']
+  styleUrls: ['./create-product.component.css'],
 })
 export class CreateProductComponent implements OnInit {
+  formsField = {
+    title: '',
+  };
+  public form = new FormGroup({
+    title: new FormControl(this.formsField.title, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get title() {
+    return this.form.controls.title as FormControl;
   }
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
